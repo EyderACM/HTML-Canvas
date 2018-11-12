02f9f142e8490c2f128c4e73c0c4d15c10e8ccae
Eyder ACM <eyderacm@gmail.com>
2018-11-12 17:31:33 -0600

    add mouse  detection

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ const canvas = document.querySelector("#draw");
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.strokeStyle = '#ffe207';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+	if(!isDrawing) return;
+	console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
