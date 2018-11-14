a23710ca1336d739e32999b8a214183d482e0f7e
Eyder ACM <eyderacm@gmail.com>
2018-11-13 22:12:42 -0600

    add the ability to draw

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ let lastY = 0;
 
 function draw(e) {
 	if(!isDrawing) return;
-	console.log(e);
+	ctx.beginPath();
+	ctx.moveTo(lastX, lastY);
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	[lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
+canvas.addEventListener('mousedown', (e) => {
+	isDrawing = true;
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
