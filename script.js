80fcc4353e07cd29e45df0b8ac13c32a7c0f97f1
Eyder ACM <eyderacm@gmail.com>
2018-11-14 21:42:23 -0600

    make pen bigger

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#ffe207';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+
 
 function draw(e) {
 	if(!isDrawing) return;
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
 	ctx.beginPath();
 	ctx.moveTo(lastX, lastY);
 	ctx.lineTo(e.offsetX, e.offsetY);
@@ -27,3 +31,4 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
